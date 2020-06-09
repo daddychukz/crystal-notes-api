a25362e0dfdfc79b3a292ac6bfcb2914591b81b0
daddychukz <durugo_chuks@yahoo.com>
2020-06-09 10:02:45 +0100

    configure serverless api

---
+++ functions/update.js
@@ -0,0 +1,25 @@
+import handler from "../libs/handler";
+import dynamoDb from "../libs/dynamodb";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET content = :content, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":attachment": data.attachment || null,
+      ":content": data.content || null
+    },
+    ReturnValues: "ALL_NEW"
+  };
+
+  await dynamoDb.update(params);
+
+  return { status: true };
+});
